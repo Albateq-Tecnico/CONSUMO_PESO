7fbb2aa64cc5b95037694c0537fcb607b1545fec
Juan Carlos Leyton Fernandez <juan.leyton@albateq.local>
2025-09-17 10:32:57 -0500

    Actualizar textos: 'Consumo Acumulado Real en Gramos' y 'Peso Real en Gramos'

--- scripts/app.py
+++ scripts/app.py
@@ -31,8 +31,8 @@ sexos = sorted(df_cons_peso['SEXO'].unique())
 raza = st.selectbox('Seleccione la Raza (Línea Genética):', razas)
 sexo = st.selectbox('Seleccione el Sexo:', sexos)
 dia = st.number_input('Día', min_value=14, value=14, step=1, format="%d")
-consumo_real = st.number_input('Consumo Acumulado Real', min_value=0.01, value=0.01)
-peso_real = st.number_input('Peso Real', min_value=0.01, value=0.01)
+consumo_real = st.number_input('Consumo Acumulado Real en Gramos', min_value=0.01, value=0.01)
+peso_real = st.number_input('Peso Real en Gramos', min_value=0.01, value=0.01)
 
 def get_poly_coeffs(df, raza, sexo):
     row = df[(df['RAZA'] == raza) & (df['SEXO'] == sexo)].iloc[0]
